1ce69651a75c153c23a5c18fbb40eb3b039c5857
nazarov-dev <nazarov.dev@gmail.com>
2023-02-25 16:38:46 +0200

    Caspio DB test

--- vue.config.js
+++ vue.config.js
@@ -9,6 +9,7 @@ module.exports = {
     filenameHashing: false,
 
     devServer: {
+        // port: 3005,
         proxy: {
             "/api/*": {
                 target: "http://localhost:3000",
